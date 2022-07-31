068ef0ccf433786f92d16fa483077e0c09dd4f68
Dibar <dibux9@gmail.com>
2022-07-31 15:44:24 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,78 @@
+import category.Category;
+import discount.Discount;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        DataGenerator.createCustomer();
+        DataGenerator.createCategory();
+        DataGenerator.createProduct();
+        DataGenerator.createBalance();
+        DataGenerator.createDiscount();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Select Customer:");
+        for (int i = 0; i < StaticConstants.CUSTOMER_LIST.size(); i++) {
+            System.out.println("Type " + i + " for customer: " + StaticConstants.CUSTOMER_LIST.get(i).getUserName());
+        }
+
+        Customer customer = StaticConstants.CUSTOMER_LIST.get(scanner.nextInt());
+
+        while(true){
+            System.out.println("What would you like to do? Just type id for selection");
+
+            for (int i = 0; i < prepareMenuOptions().length; i++) {
+                System.out.println(i + "-" + prepareMenuOptions()[i]);
+            }
+
+            int menuSelection = scanner.nextInt();
+
+            switch (menuSelection){
+                case 0:   // list categories
+                    for (Category each : StaticConstants.CATEGORY_LIST) {
+                        System.out.println("Category Code: " + each.generateCategoryCode() + " Category name:" + each.getName());
+                    }
+                    break;
+                case 1:  // list products  // product name, product category name
+                    try{
+                        for (Product product : StaticConstants.PRODUCT_LIST) {
+                            System.out.println("Product Name: " + product.getName() + ", Product Category Name: " + product.getCategoryName());
+                        }
+                    }catch (Exception e){
+                        System.out.println("Product could not be printed because category not found for product name: " + e.getMessage().split(",")[1]);
+                    }
+
+                    break;
+                case 2: // list discounts
+                    for (Discount discount : StaticConstants.DISCOUNT_LIST) {
+                        System.out.println("Discount Name: " + discount.getName() + ", Discount threshold amount: " + discount.getThresholdAmount());
+                    }
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+
+            }
+        }
+
+    }
+
+    private static String[] prepareMenuOptions(){
+        return new String[]{"List Categories", "List Products", "List Discount", "See Balance", "Add Balance",
+                "Place an Order", "See Cart", "See order details", "See your address", "Close App"};
+    }
+}
+
